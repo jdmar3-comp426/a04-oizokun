d48ada78336921cf666e3e46a20ffb71ee6abcd8
ose <oseizo@live.unc.edu>
2021-12-03 02:58:13 -0500

    update

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ app.patch("/app/update/user/:id", (req, res) => {
   const stmt = db.prepare(
     "UPDATE userinfo SET user = COALESCE(?,user), pass = COALESCE(?,pass) WHERE id = ?"
   );
-  const info = stmt.run(req.body.user, md5(req.body.pass));
+  const info = stmt.run(req.body.user, req.body.pass, req.params.id);
   res.status(200).json({
     message:
       info.changes + " record updated: ID " + info.lastInsertRowid + " (201)",
